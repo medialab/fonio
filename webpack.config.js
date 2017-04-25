ea3e322e88023c96ee0ddf5ac81c60d5cdc1e7dc
Robin de Mourat <robin.demourat@gmail.com>
2017-04-25 11:28:41 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,35 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+        // use: [
+        //     'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+        //     'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        // ]
+         use: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name:'assets/[name].[ext]'
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }]
       },
       {
         test: /\.scss$/,
@@ -25,11 +50,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify('production')
+    //   }
+    // }),
     // new webpack.optimize.UglifyJsPlugin()
   ]
 };
