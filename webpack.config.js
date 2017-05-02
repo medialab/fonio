c13fa42fd58acc6010edeb7c2b650998fffca075
Robin de Mourat <robin.demourat@gmail.com>
2017-05-02 11:07:59 +0200

    reenable minification

--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,11 @@ module.exports = {
     ]
   },
   plugins: [
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify('production')
-    //   }
-    // }),
-    // new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 };
