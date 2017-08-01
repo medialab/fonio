4d5f5e02ea99125780041088fc05de90a1de6635
Robin de Mourat <robin.demourat@gmail.com>
2017-08-01 14:48:50 +0200

    hack around a citation-js bug with new authors (comments in code)

--- src/components/BibRefsEditor/BibRefsEditor.js
+++ src/components/BibRefsEditor/BibRefsEditor.js
@@ -144,18 +144,20 @@ export default class BibRefsEditor extends Component {
       if (referenceId === undefined) {
         const refs = references || [];
         return this.props.onChange([...refs, {
-          type: '',
+          type: 'book',
           title: '',
-          // commented because it makes citation-js crash
-          // when creating a new reference in the interface
-          // there is an error in their lib at src/get/bibtex/label line 14 ("if (author)" should be "if (author && author[0])"
-          // @todo: make a PR to citation-js to be able to have authors in default new citation
-          // author: [{
-          //   family: '',
-          //   given: '',
-          //   literal: '',
-          //   id: generateId()
-          // }],
+          author: [{
+            // need to provide a truthy value for author's name parts because if not citation-js crashes
+            // (pb in citation-js/src/parse/csl.js - function "correctName"
+            // evaluates the validity of an author entry
+            // by checking if it contains a name.family and name.given
+            // but it does it with a truthy test while it should test if undefined instead)
+            // @todo: make a PR to citation-js to be able to have empty authors name entries in default new citation
+            family: ' ',
+            given: ' ',
+            // literal: '',
+            id: generateId()
+          }],
           id: generateId()
         }]);
       }
