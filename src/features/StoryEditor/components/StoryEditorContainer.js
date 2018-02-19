eddd1c9fce2e7632a7f2df022fd16e3d06ec6866
Robin de Mourat <robin.demourat@gmail.com>
2018-02-19 16:54:19 +0100

    refactor summonAsset method as a shared util

--- src/features/StoryEditor/components/StoryEditorContainer.js
+++ src/features/StoryEditor/components/StoryEditorContainer.js
@@ -10,10 +10,6 @@ import {connect} from 'react-redux';
 import {setLanguage} from 'redux-i18n';
 import {v4 as genId} from 'uuid';
 
-import {
-  convertToRaw
-} from 'draft-js';
-
 import StoryEditorLayout from './StoryEditorLayout';
 import * as duck from '../duck';
 import * as managerDuck from '../../StoriesManager/duck';
@@ -39,14 +35,12 @@ import {
 } from '../../ResourcesManager/duck';
 
 import {
-  // insertAssetInEditor,
-  insertInlineContextualization,
-  insertBlockContextualization,
-} from '../../../helpers/draftUtils';
-
-import {
   createDefaultSection
 } from '../../../helpers/schemaUtils';
+
+import {
+  summonAsset
+} from '../../../helpers/assetsUtils';
 
 
 /**
@@ -140,120 +134,19 @@ class EditorContainer extends Component {
     this.props.actions.createSection(this.props.activeStoryId, id, section, true);
   }
 
-
-  /**
-   * Handle the process of creating a new asset in the active story.
-   * This implies three operations :
-   * - create a contextualizer (which defines a way of materializing the resource)
-   * - create contextualization (unique combination of a contextualizer, a section and a resource)
-   * - insert an entity linked to the contextualization in the proper draft-js content state (main or note of the section)
-   * @param {string} contentId - the id of editor to target ('main' or note id)
-   * @param {string} resourceId - id of the resource to summon
-   */
-  summonAsset = (contentId, resourceId) => {
-    // todo: this is a duplicate with ResourcesManagerContainer.summonAsset
-    // so this should be refactored as a shared helper
-    // or some other solution should be found not to repeat it
-    const {
-      activeStoryId,
-      activeStory,
-      activeSectionId,
-      editorStates,
-      actions,
-    } = this.props;
-
-    const {
-      createContextualizer,
-      createContextualization,
-      updateDraftEditorState,
-      updateSection,
-    } = actions;
-
-    const activeSection = activeStory.sections[activeSectionId];
-    const resource = activeStory.resources[resourceId];
-
-    // 1. create contextualizer
-    // question: why isn't the contextualizer
-    // data directly embedded in the contextualization data ?
-    // answer: that way we can envisage for the future to
-    // give users a possibility to reuse the same contextualizer
-    // for different resources (e.g. comparating datasets)
-    // and we can handle multi-modality in a smarter way.
-
-    // todo : consume model to do that
-    const contextualizerId = genId();
-    const contextualizer = {
-      id: contextualizerId,
-      type: resource.metadata.type,
-    };
-    createContextualizer(activeStoryId, contextualizerId, contextualizer);
-
-    // choose if inline or block
-    // todo: for now we infer from the resource type whether contextualization
-    // must be in block or inline mode.
-    // but we could choose to let the user decide
-    // (e.g. 1: a 'bib' reference in block mode
-    // could be the full reference version of the reference)
-    // (e.g. 2: a 'quinoa presentation' reference in inline mode
-    // could be an academic-like short citation of this reference)
-
-    // todo: choose that from resource model
-    const insertionType = resource.metadata.type === 'bib' || resource.metadata.type === 'glossary' ? 'inline' : 'block';
-
-    // 2. create contextualization
-    const contextualizationId = genId();
-    const contextualization = {
-      id: contextualizationId,
-      resourceId,
-      contextualizerId,
-      sectionId: activeSectionId
-    };
-    createContextualization(activeStoryId, contextualizationId, contextualization);
-
-    // 3. update the proper editor state
-    const editorStateId = contentId === 'main' ? activeSectionId : contentId;
-    const editorState = editorStates[editorStateId];
-    // update related editor state
-    const newEditorState = insertionType === 'block' ?
-      insertBlockContextualization(editorState, contextualization, contextualizer, resource) :
-      insertInlineContextualization(editorState, contextualization, contextualizer, resource);
-    // update immutable editor state
-    updateDraftEditorState(editorStateId, newEditorState);
-    // update serialized editor state
-    let newSection;
-    if (contentId === 'main') {
-      newSection = {
-        ...activeSection,
-        contents: convertToRaw(newEditorState.getCurrentContent())
-      };
-    }
-    else {
-      newSection = {
-        ...activeSection,
-        notes: {
-          ...activeSection.notes,
-          [contentId]: {
-            ...activeSection.notes[contentId],
-            editorState: convertToRaw(newEditorState.getCurrentContent())
-          }
-        }
-      };
-    }
-    updateSection(activeStoryId, activeSectionId, newSection);
-  }
-
   /**
    * Renders the component
    * @return {ReactElement} component - the component
    */
   render() {
+    const onSummonAsset = (contentId, resourceId) => summonAsset(contentId, resourceId, this.props);
     return (
       <StoryEditorLayout
         {...this.props}
         openSettings={this.openSettings}
         closeAndResetDialog={this.closeAndResetDialog}
         onCreateNewSection={this.createNewSection}
-        summonAsset={this.summonAsset} />
+        summonAsset={onSummonAsset} />
     );
   }
 }
