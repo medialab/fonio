d4d139424fe3969b2f517cd8d41e2d142d736642
mydu <dumengying1989@gmail.com>
2018-03-05 12:24:05 +0100

    remove persistentStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -11,7 +11,6 @@ import {
 import rootReducer from './rootReducer';
 import promiseMiddleware from './promiseMiddleware';
 import autoSaveMiddleware from './autoSaveMiddleware';
-import {persistentStore} from 'redux-pouchdb';
 import {loadingBarMiddleware} from 'react-redux-loading-bar';
 
 const PouchDB = require('pouchdb').default;
@@ -36,10 +35,8 @@ export default function configureStore (initialState = {}) {
   const createStoreWithMiddleware = window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(
     // related middlewares
     middleware,
-    // connections to pouchdb
-    persistentStore(db),
     // connection to redux dev tools
-    window.__REDUX_DEVTOOLS_EXTENSION__())(createStore) : compose(middleware, persistentStore(db))(createStore);
+    window.__REDUX_DEVTOOLS_EXTENSION__())(createStore) : compose(middleware)(createStore);
 
   const store = createStoreWithMiddleware(
     rootReducer,
