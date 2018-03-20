57183899fe364a250d0b9eb168efeeac341b5cad
mydu <dumengying1989@gmail.com>
2018-03-20 14:05:59 +0100

    prevent reloading when promise not finish

--- src/Application.js
+++ src/Application.js
@@ -7,7 +7,9 @@
  * Root component of the application.
  * @module fonio
  */
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -22,27 +24,55 @@ import GlobalUi from './features/GlobalUi/components/GlobalUiContainer.js';
 import StoryView from './features/StoryView/components/StoryViewContainer.js';
 import StoriesManager from './features/StoriesManager/components/StoriesManagerContainer';
 
-
+@connect(
+  state => ({
+    loadingBar: state.loadingBar.default,
+  })
+)
 /**
  * Renders the whole fonio application
  * @return {ReactComponent} component
  */
-const Application = ({}) => (
-  <Router>
-    <div id="wrapper" className="fonio">
-      <GlobalUi />
-      <Switch>
-        <Route exact path="/" component={StoriesManager} />
-        <Route path="/story/:id/:mode?" render={(props) => (<StoryView {...props} />)} />
-        <Route render={(props) => (
-          // TODO: loading/error page
-          <h2>
-            No match for {props.location.pathname}, go back to <Link to="/">Home page</Link>
-          </h2>
-        )} />
-      </Switch>
-    </div>
-  </Router>
-);
+export default class Application extends Component {
 
-export default Application;
+  /**
+   * constructorstorestore
+   * @param {object} props - properties given to instance at instanciation
+   */
+  constructor(props) {
+    super(props);
+    this.confirmExit = this.confirmExit.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.confirmExit);
+  }
+
+  confirmExit(e) {
+    const {loadingBar} = this.props;
+    if (loadingBar > 0) {
+      const confirmationMessage = '\o/';
+      e.returnValue = confirmationMessage; // Gecko, Trident, Chrome 34+
+      return confirmationMessage;
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <div id="wrapper" className="fonio">
+          <GlobalUi />
+          <Switch>
+            <Route exact path="/" component={StoriesManager} />
+            <Route path="/story/:id/:mode?" render={(props) => (<StoryView {...props} />)} />
+            <Route render={(props) => (
+              // TODO: loading/error page
+              <h2>
+                No match for {props.location.pathname}, go back to <Link to="/">Home page</Link>
+              </h2>
+            )} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
