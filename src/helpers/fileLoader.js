d6b0d3777317bb78babbf06cfa1eaf0c6d1cb921
mydu <dumengying1989@gmail.com>
2018-06-12 10:59:29 +0200

    import and duplicate story

--- src/helpers/fileLoader.js
+++ src/helpers/fileLoader.js
@@ -18,17 +18,17 @@ export function validateFileExtensionForVisType (fileName = '', visualizationMod
  * @param {File} fileToRead - the file to read
  * @param {function} callback
  */
-export function getFileAsText(fileToRead, callback) {
-  let reader = new FileReader();
-  // Handle errors load
-  reader.onload = (event) => {
-    callback(null, event.target.result);
-    reader = undefined;
-  };
-  reader.onerror = (event) => {
-    callback(event.target.error);
-    reader = undefined;
-  };
-  // Read file into memory as UTF-8
-  reader.readAsText(fileToRead);
+export function getFileAsText(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target.result);
+      reader = undefined;
+    };
+    reader.onerror = (event) => {
+      reject(event.target.error);
+      reader = undefined;
+    };
+    return reader.readAsText(file);
+  });
 }
