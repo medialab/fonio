e4f2d2ae5f2a6011276d700fb6fa80d3595fd25a
mydu <dumengying1989@gmail.com>
2018-07-05 15:00:18 +0200

    add className for delete icon

--- src/components/AuthorsManager/AuthorsManager.js
+++ src/components/AuthorsManager/AuthorsManager.js
@@ -65,7 +65,7 @@ const AuthorsManager = ({
                 <Icon isSize="small" isAlign="left">
                   <span className="fa fa-user" aria-hidden="true" />
                 </Icon>
-                <Icon isSize="small" isAlign="right" style={{pointerEvents: 'auto'}}>
+                <Icon isSize="small" isAlign="right" className="is-clickable">
                   <Delete onClick={onRemoveAuthor} />
                 </Icon>
               </Control>
