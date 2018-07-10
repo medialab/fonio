c9e4a09b5ccbf7778766ccd5c404d78ca6cea3b3
Robin de Mourat <robin.demourat@gmail.com>
2018-07-10 10:39:27 +0200

    improve section view header mini design fix

--- src/features/SectionView/components/MainSectionColumn.js
+++ src/features/SectionView/components/MainSectionColumn.js
@@ -335,7 +335,7 @@ const MainSectionColumn = ({
                   isSize={editorWidth}
                   isOffset={editorX} isWrapper>
                   {/* editor header*/}
-                  <StretchedLayoutContainer isFluid isDirection={mainColumnMode !== 'edition' ? 'vertical' : 'horizontal'}>
+                  <StretchedLayoutContainer isFluid isDirection={/*mainColumnMode !== 'edition' ? 'vertical' : 'horizontal'*/'vertical'}>
                     <StretchedLayoutItem isFlex={1}>
                       <Title isSize={2}>
                         {abbrevString(section.metadata.title, 20)}
