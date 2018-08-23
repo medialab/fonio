0a4f65efaf852055db7a64c61f830ef6950e242f
Robin de Mourat <robin.demourat@gmail.com>
2018-08-23 14:09:15 +0200

    improve preview encapsulation

--- src/features/DesignView/components/MainDesignColumn.js
+++ src/features/DesignView/components/MainDesignColumn.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import StoryPlayer from 'quinoa-story-player';
+import {render} from 'react-dom';
 
 import {
   Column,
 } from 'quinoa-design-library/components/';
 
 import {processCustomCss} from '../../../helpers/postcss';
+
+class PreviewWrapper extends Component {
+
+  componentDidMount = () => {
+    setTimeout(() => this.update(this.props));
+  }
+  componentWillReceiveProps = nextProps => {
+    if (this.props.story !== nextProps.story || this.props.lang !== nextProps.lang) {
+      setTimeout(() => this.update(this.props));
+    }
+  }
+
+  update = (props) => {
+    const {story, lang} = props;
+    const contentDocument = this.iframe && this.iframe.contentDocument;
+    const contentWindow = this.iframe && this.iframe.contentWindow;
+    if (contentDocument) {
+      let mount = contentDocument.getElementById('mount');
+      if (!mount) {
+        mount = contentDocument.createElement('div');
+        mount.id = 'mount';
+        contentDocument.body.appendChild(mount);
+      }
+      render(
+        <StoryPlayer
+          locale={lang}
+          story={{
+            ...story,
+            settings: {
+              ...story.settings,
+              css: processCustomCss(story.settings.css)
+            }
+          
+          }} 
+          usedDocument={contentDocument}
+          usedWindow={contentWindow}
+        />, mount);
+    }
+  }
+  render = () => {
+    const bindRef = iframe => {
+      this.iframe = iframe;
+    }
+    
+    return <iframe style={{width: '100%', height: '100%'}} ref={bindRef} />;
+  }
+}
 
 
 const MainDesignColumn = ({
@@ -16,7 +64,8 @@ const MainDesignColumn = ({
 
   return (
     <Column isSize={'fullwidth'} style={{position: 'relative'}}>
-      <StoryPlayer
+      {<PreviewWrapper story={story} lang={lang} />}
+      {/*<StoryPlayer
         locale={lang}
         story={{
           ...story,
@@ -24,7 +73,7 @@ const MainDesignColumn = ({
             ...story.settings,
             css: processCustomCss(story.settings.css)
           }
-        }} />
+        }} />*/}
     </Column>
   );
 };
