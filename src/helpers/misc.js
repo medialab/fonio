c5f58a7c8a4cdcfef75c2e98e3c901b8829b2887
Robin de Mourat <robin.demourat@gmail.com>
2018-08-29 11:13:17 +0200

    smallfix

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -1,5 +1,5 @@
 
-export const abbrevString = (str, maxLength = 10) => {
+export const abbrevString = (str = '', maxLength = 10) => {
   if (str.length > maxLength) {
    return str.substr(0, maxLength) + '...';
   }
