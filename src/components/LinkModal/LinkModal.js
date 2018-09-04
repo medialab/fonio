f1854885fa105ff2071a6f93fc8387800ec46038
Robin de Mourat <robin.demourat@gmail.com>
2018-09-04 17:45:32 +0200

    fix quick link visual bug c#235

--- src/components/LinkModal/LinkModal.js
+++ src/components/LinkModal/LinkModal.js
@@ -14,6 +14,7 @@ import {
 } from 'quinoa-design-library/components/';
 
 import {translateNameSpacer} from '../../helpers/translateUtils';
+import {abbrevString} from '../../helpers/misc';
 
 import icons from 'quinoa-design-library/src/themes/millet/icons';
 
@@ -110,10 +111,16 @@ class LinkModal extends Component {
                             .map(resource => ({
                             id: resource.id,
                             label: (
-                              <span style={{display: 'flex', flexFlow: 'row nowrap', alignItems: 'center'}}>
-                                <Image style={{display: 'inline-block', marginRight: '1em'}} isSize={'16x16'} src={icons.webpage.black.svg} />
-                                <span>
-                                  {`${resource.metadata.title} (${resource.data.url})`}
+                              <span 
+
+                                style={{display: 'flex', flexFlow: 'row nowrap', alignItems: 'center'}}>
+                                <Image 
+                                  style={{display: 'inline-block', marginRight: '1em'}} 
+                                  isSize={'16x16'} 
+                                  src={icons.webpage.black.svg} 
+                                />
+                                <span >
+                                  {`${abbrevString(resource.metadata.title, 30)} (${abbrevString(resource.data.url, 30)})`}
                                 </span>
                               </span>
                               )
