8890cfdfba9fd8ec848be99ffbb782e25af34711
jeremy richard <jeremy.richard@sciencespo.fr>
2018-11-29 12:57:50 +0100

    :whale: fix nginx resolver issue when backend address change

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -5,7 +5,8 @@ sed "s;@@URL_PREFIX@@;${URL_PREFIX};" /fonio/build/bundle.js.template > /fonio/b
 envsubst < /fonio/index.prod.html.template > /fonio/index.html
 
 # Templating the NGINX conf
-envsubst '\$HOST \$PORT \$MAX_STORY_SIZE' < /etc/nginx/conf.d/docker.template > /etc/nginx/conf.d/default.conf
+export NS=$(awk '/^nameserver/{print $2}' /etc/resolv.conf)
+envsubst '\$NS \$QUINOA_HOST \$QUINOA_PORT \$MAX_STORY_SIZE' < /etc/nginx/conf.d/docker.template > /etc/nginx/conf.d/default.conf
 
 # No daemon
 nginx -g 'daemon off;'
