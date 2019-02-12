17453b2e454b8f8767d7ee8b32c77895741bddb6
mydu <dumengying1989@gmail.com>
2019-02-12 11:19:58 +0100

    add backward/forward cases for handleCopy

--- src/helpers/clipboardUtils/__mocks__/services.js
+++ src/helpers/clipboardUtils/__mocks__/services.js
@@ -7,7 +7,7 @@ import {
   convertFromRaw
 } from 'draft-js';
 
-const getEditorStates = ( story ) => {
+const getEditorStates = ( { story, isBackward } ) => {
   const { sections, sectionsOrder } = story;
   const activeSectionId = sectionsOrder[0];
   const { contents } = sections[activeSectionId];
@@ -15,21 +15,27 @@ const getEditorStates = ( story ) => {
   /*
    * step 1: initialize editorState with convertFromRaw()
    * step 2: move selection/focus to the end of the editor
-   * step 3: get first block and first key
-   * step 4: change selection from start to end
+   * step 3: get first/last block's key
+   * step 4: change selection from start/end to end/start
    */
   const editorState = EditorState.moveFocusToEnd(
     EditorState.createWithContent( convertFromRaw( contents ) )
   );
-
-  const content = editorState.getCurrentContent();
-  const firstBlock = content.getFirstBlock();
+  const selection = editorState.getSelection();
+  const firstBlock = editorState.getCurrentContent().getFirstBlock();
   const firstKey = firstBlock.getKey();
+  const lastBlock = editorState.getCurrentContent().getLastBlock();
+  const lastKey = lastBlock.getKey();
+  const endOffset = selection.getFocusOffset();
 
-  const newSelection = editorState.getSelection().merge( {
-    anchorOffset: 0,
-    anchorKey: firstKey
+  const newSelection = selection.merge( {
+    anchorOffset: isBackward ? endOffset : 0,
+    anchorKey: isBackward ? firstKey : lastKey,
+    focusOffset: isBackward ? 0 : endOffset,
+    focusKey: isBackward ? lastKey : firstKey,
+    isBackward
   } );
+
   const newEditorState = EditorState.acceptSelection( editorState, newSelection );
 
   return {
--- src/helpers/clipboardUtils/handleCopy.spec.js
+++ src/helpers/clipboardUtils/handleCopy.spec.js
@@ -15,9 +15,12 @@ import {
 } from './handleCopy';
 
 describe( 'unit test for handleCopy', () => {
-  describe( 'unit test for computeCopiedData', () => {
-    it( 'editor states creation', () => {
-      const editorStates = services.getEditorStates( story );
+  describe( 'unit test for computeCopiedData()', () => {
+    it( 'forward selection copy', () => {
+      const editorStates = services.getEditorStates( {
+        story,
+        isBackward: false
+      } );
       const copiedEditor = {
         editorFocus: 'main',
         editorStates,
@@ -26,7 +29,47 @@ describe( 'unit test for handleCopy', () => {
         },
         story,
       };
-      expect( computeCopiedData( copiedEditor ) ).toBeDefined();
+
+      const {
+        copiedContextualizations,
+        copiedContextualizers,
+        copiedResources,
+      } = computeCopiedData( copiedEditor );
+      const expectedContextualizations = Object.keys( story.contextualizations ).map( ( id ) => story.contextualizations[id] );
+      const expectedContextualizers = Object.keys( story.contextualizers ).map( ( id ) => story.contextualizers[id] );
+      const expectedResources = Object.keys( story.resources ).map( ( id ) => story.resources[id] );
+
+      expect( copiedContextualizations ).toEqual( expectedContextualizations );
+      expect( copiedContextualizers ).toEqual( expectedContextualizers );
+      expect( copiedResources ).toEqual( expectedResources );
+    } );
+
+    it( 'backward selection copy', () => {
+      const editorStates = services.getEditorStates( {
+        story,
+        isBackward: true
+      } );
+      const copiedEditor = {
+        editorFocus: 'main',
+        editorStates,
+        activeSection: {
+          id: story.sectionsOrder[0]
+        },
+        story,
+      };
+
+      const {
+        copiedContextualizations,
+        copiedContextualizers,
+        copiedResources,
+      } = computeCopiedData( copiedEditor );
+      const expectedContextualizations = Object.keys( story.contextualizations ).map( ( id ) => story.contextualizations[id] );
+      const expectedContextualizers = Object.keys( story.contextualizers ).map( ( id ) => story.contextualizers[id] );
+      const expectedResources = Object.keys( story.resources ).map( ( id ) => story.resources[id] );
+
+      expect( copiedContextualizations ).toEqual( expectedContextualizations );
+      expect( copiedContextualizers ).toEqual( expectedContextualizers );
+      expect( copiedResources ).toEqual( expectedResources );
     } );
   } );
 } );
